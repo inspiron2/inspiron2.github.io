531f6e01729db9c298c080abff10d57894b9f9ea
inspiron2 <swagod@gmail.com>
2017-06-05 11:46:14 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 // Empty JS for your own code to be here
+document.querySelector("#vibrate-one-second").addEventListener("click", function () {
+    navigator.mozVibrate(1000);
+}, false);
+
+document.querySelector("#vibrate-twice").addEventListener("click", function () {
+    navigator.mozVibrate([200, 100, 200, 100]);
+}, false);
+
+
+document.querySelector("#vibrate-long-time").addEventListener("click", function () {
+    navigator.mozVibrate(5000);
+}, false);
+
+document.querySelector("#vibrate-off").addEventListener("click", function () {
+     navigator.mozVibrate(0);
+ }, false);
