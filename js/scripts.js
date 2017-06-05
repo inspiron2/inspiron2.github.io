dc5ff4347ec91c73ac62f6d370e727f98b1042df
inspiron2 <swagod@gmail.com>
2017-06-05 12:23:57 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
 if (navigator.vibrate) {
+	alert("ya");
 	$("#vibrate-one-second").on("click", function () {
-        navigator.vibrate(200);
-    });
+	alert("ya1");
+		
+		navigator.vibrate(200);
+	});
 	$("#vibrate-twice").on("click", function () {
-	    navigator.vibrate([200, 100]);
+	alert("ya2");
+		
+		navigator.vibrate([200, 100]);
 	});
 	$("#vibrate-long-time").on("click", function () {
-    navigator.vibrate(5000);
-});
+		navigator.vibrate(2000);
+	});
 
-$("#vibrate-off").on("click", function () {
-     navigator.vibrate(0);
- });
+	$("#vibrate-off").on("click", function () {
+	alert("ya4");
+		
+		navigator.vibrate(0);
+	});
 }
 
 
